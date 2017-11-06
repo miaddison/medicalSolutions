e033c0ba27b8cb3957dd6208ecc78d3efed674c8
Merna Addison <merna.addison@gmail.com>
2017-11-06 10:17:26 -0600

    first version written by James

---
+++ medicalSolutionsGeneralAsPossible.cpp
@@ -0,0 +1,28 @@
+// medicalSolutionsGeneralAsPossible.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<iostream>
+#include<string>
+using namespace std;
+
+class patient 
+{
+	string name;
+	string dateOfBirth;
+	string address;
+	string phoneNumber;
+	string currentMedications;
+	double height, weight;
+	string reasonForVisit;
+	string allergies;
+	int currentPainLevel;
+	bool safeInHome;
+
+
+};
+int main()
+{
+    return 0;
+}
+
